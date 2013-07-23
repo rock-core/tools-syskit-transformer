36e5781acf6097c6da5d32d4d4cf96edb2467bba
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-07-23 16:45:32 +0200

    import the syskit-related test suite from tests/orogen/transformer
    It still requires the orogen project to run, but at least we have it
    at the right place.

--- test/test_transformer.rb
+++ test/test_transformer.rb
@@ -1,72 +1,63 @@
-$LOAD_PATH.unshift(File.expand_path(File.join('..', 'lib'), File.dirname(__FILE__)))
-require 'transformer'
-require 'test/unit'
-require 'eigen'
-require 'pp'
+require 'syskit/test'
+require 'transformer/syskit/test'
 
-class TC_Transformer < Test::Unit::TestCase
-    attr_reader :trsf
-    def conf; trsf.conf end
+describe Transformer do
+    include Transformer::SyskitPlugin::SelfTest
 
-    def setup
-        @trsf = Transformer::TransformationManager.new
+    before do
+        Roby.app.using_task_library 'test_transformer'
+        ::Robot.logger.level = Logger::WARN
+        Syskit.conf.use_deployments_from "test_transformer"
     end
 
-    def test_simple_transformation_chain
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
+    def test_it_detects_conflicts_between_frame_selection_and_device_definition
+        dev_m = Syskit::Device.new_submodel do
+            output_port 'trsf', 'base/samples/RigidBodyState'
+        end
+        TestTransformer::ConfigurableTransformProducer.driver_for dev_m, :as => 'transformation'
+        dev = robot.device(dev_m, :as => 'device').
+            frame_transform('test_from' => 'test_to').
+            period(0.1)
 
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_low", "servo_high", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("laser", "camera", Eigen::Vector3.new(0, 0, 0))
-        ]
-        chain = trsf.transformation_chain("body", "laser")
-        assert_equal(transforms[0, 3], chain.links)
-        assert_equal([false, false, false], chain.inversions)
+        req = dev.to_instance_requirements.
+            use_deployments('configurable_transform_producer').
+            use_frames('from' => 'other_from')
+
+        plan.add_mission(mission = req.as_plan)
+        mission = mission.as_service
+
+        inhibit_fatal_messages do
+            assert_raises(Transformer::FrameSelectionConflict) do
+                deploy(mission)
+            end
+        end
     end
 
-    def test_transformation_chain_with_inversions
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
-
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("laser", "camera", Eigen::Vector3.new(0, 0, 0))
-        ]
-        chain = trsf.transformation_chain("body", "laser")
-        assert_equal(transforms[0, 3], chain.links)
-        assert_equal([false, true, false], chain.inversions)
+    def test_link_task_frame_to_device_frame_using_frame
+        device_m = Syskit::Device.new_submodel do
+            output_port "data", "test_transformer/Result"
+        end
+        TestTransformer::BasicBehaviour.driver_for device_m, :as => 'driver'
+        dev = robot.device(device_m, :as => 'dev').frame('test_frame')
+        plan.add_mission(
+            mission = TestTransformer::BasicBehaviour.with_arguments("driver_dev" => dev).as_plan)
+        mission = mission.as_service
+        deploy(mission, :validate_network => false)
+        assert_equal "test_frame", mission.selected_frames['output']
     end
 
-    def test_transformation_chain_with_loop_ignored
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
-
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("servo_high", "body", Eigen::Vector3.new(0, 0, 0))
-        ]
-        chain = trsf.transformation_chain("body", "laser")
-        assert_equal([transforms[3], transforms[2]], chain.links)
-        assert_equal([true, false], chain.inversions)
-    end
-
-    def test_transformation_chain_with_loop_shortest_path
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
-
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("servo_high", "body", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("laser", "camera", Eigen::Vector3.new(0, 0, 0))
-        ]
-        chain = trsf.transformation_chain("body", "laser")
-        assert_equal([transforms[3], transforms[2]], chain.links)
-        assert_equal([true, false], chain.inversions)
+    def test_link_task_frame_to_device_frame_using_transform
+        device_m = Syskit::Device.new_submodel do
+            output_port "data", "base/samples/RigidBodyState"
+        end
+        TestTransformer::ConfigurableTransformProducer.driver_for device_m, :as => 'driver'
+        dev = robot.device(device_m, :as => 'dev').frame_transform('test_source' => 'test_target')
+        plan.add_mission(
+            mission = TestTransformer::ConfigurableTransformProducer.with_arguments("driver_dev" => dev).use_deployments('configurable_transform_producer').as_plan)
+        mission = mission.as_service
+        deploy(mission, :validate_network => false)
+        assert_equal "test_source", mission.selected_frames['from']
+        assert_equal "test_target", mission.selected_frames['to']
     end
 end
 
